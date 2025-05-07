97d6e748bce4c2ef20dbe7bc206bbd96e9a58650
Heikel Molina <qbah3h@gmail.com>
2025-05-07 17:54:55 -0300

    Update on language

--- src/models/curriculum.js
+++ src/models/curriculum.js
@@ -60,7 +60,7 @@ const SectionSchema = new mongoose.Schema({
 // Curriculum schema
 const curriculumSchema = new mongoose.Schema({
   from: { type: String, required: true },
-  language: {type: String, default: 'es'},
+  language: {type: String, default: ''},
   // userMessage: { type: String, default: '' },
   // prevChatbotMessage: { type: String, default: '' },
   newChatbotMessage: { type: String, default: '' },
