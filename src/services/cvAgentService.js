4fc8883b290f69a6b12145311011a8b0c9f98792
Heikel Molina <qbah3h@gmail.com>
2025-05-07 12:26:41 -0300

    Update on language

--- src/services/cvAgentService.js
+++ src/services/cvAgentService.js
@@ -260,6 +260,7 @@ async function handlePDFGeneration(curriculum, profileImage, conversation) {
   try {
     // Generate PDF when conversation is completed
     const pdfStartTime = Date.now();
+    curriculum.language = 'en';
     const pdfResult = await generatePDF(curriculum, profileImage);
     const pdfData = pdfResult.pdfBuffer;
     const pdfFilename = pdfResult.filename;
